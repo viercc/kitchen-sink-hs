aaa470c6f0fc04e5a9ab2da46d89bc397dadd35c
Koji Miyazato <viercc@gmail.com>
2019-11-19 23:41:30 +0900

    More strange monads

--- monads-collection/src/Monad/Esoteric.hs
+++ monads-collection/src/Monad/Esoteric.hs
@@ -6,11 +6,15 @@ kinds of Monad instances.
 -}
 {-# LANGUAGE DerivingVia       #-}
 {-# LANGUAGE DeriveTraversable #-}
-{-# LANGUAGE TypeOperators     #-}
 module Monad.Esoteric where
 
+import Data.Foldable
 import Control.Applicative
-import GHC.Generics ((:+:)(..))
+
+import Data.List (unfoldr)
+
+-- These are found by exhaustively searching
+-- all functions of the type (forall a. F (F a) -> F a).
 
 data Span a = Point a | Span a a
   deriving stock (Show, Read, Eq, Ord, Functor, Foldable, Traversable)
@@ -62,3 +66,84 @@ instance Monad Twist where
           (True,  False, True)  -> T True  x01 x11
           (True,  True,  False) -> T True  x00 x10
           (True,  True,  True)  -> T True  x00 x11
+
+--------------------------------
+
+-- Non empty zip(ish)-list.
+-- by treating finite list as "converging" sequence.
+-- By "converging", I mean, it repeats same value after
+-- finite elements.
+
+data Nezzle a = a :| [a]
+  deriving stock (Show, Read, Eq, Ord, Functor, Foldable, Traversable)
+  deriving (Applicative) via (WrappedMonad Nezzle)
+
+headNezzle :: Nezzle a -> a
+headNezzle (a :| _) = a
+
+tailNezzle :: Nezzle a -> Nezzle a
+tailNezzle (_ :| (a:as)) = a :| as
+tailNezzle single        = single
+
+instance Monad Nezzle where
+  return a = a :| []
+  a :| []      >>= k = k a
+  a :| (a':as) >>= k =
+    headNezzle (k a) :| toList ((a' :| as) >>= tailNezzle . k)
+
+--------------------------------
+
+-- Odd-lengthed list
+-- 
+-- Monad instance is same as []'s instance.
+-- Because concat'ing odd number of odd-lengthed lists yields
+-- odd-lengthed list, it's a monad.
+-- 
+-- "Even-lengthed" list can't have such an instance,
+-- because the length of `return` can't be even.
+data Odd a = Odd a (Even a)
+  deriving stock (Show, Read, Eq, Ord, Functor, Foldable, Traversable)
+  deriving (Applicative) via (WrappedMonad Odd)
+
+data Even a = Nil | Even a (Odd a)
+  deriving stock (Show, Read, Eq, Ord, Functor, Foldable, Traversable)
+
+plusEE :: Even a -> Even a -> Even a
+plusEE Nil ys = ys
+plusEE (Even x xs) ys = Even x (plusOE xs ys)
+
+plusOE :: Odd a -> Even a -> Odd a
+plusOE (Odd x xs) ys = Odd x (plusEE xs ys)
+
+plusEO :: Even a -> Odd a -> Odd a
+plusEO Nil ys = ys
+plusEO (Even x xs) ys = Odd x (plusOO xs ys)
+
+plusOO :: Odd a -> Odd a -> Even a
+plusOO (Odd x xs) ys = Even x (plusEO xs ys)
+
+instance Monad Odd where
+  return a = Odd a Nil
+  ma >>= k = ostep ma
+    where
+      ostep (Odd x xs) = k x `plusOE` estep xs
+      
+      estep Nil         = Nil
+      estep (Even x xs) = k x `plusOO` ostep xs
+
+-- (3n+1)-lengthed list, (4n+1)-lengthed list, ...
+-- are also Monads.
+
+newtype List3Np1 a = List3Np1 [a]
+  deriving stock (Show, Read, Eq, Ord, Functor, Foldable, Traversable)
+  -- Monad instance inherited from [] keeps invariant.
+  deriving newtype (Applicative, Monad)
+
+makeList3Np1 :: [a] -> Maybe (List3Np1 a)
+makeList3Np1 as
+  | n `rem` 3 == 1 = Just (List3Np1 as)
+  | otherwise      = Nothing
+  where n = length as
+
+makeInftyList3Np1 :: (s -> (a, s)) -> s -> List3Np1 a
+makeInftyList3Np1 f s = List3Np1 (unfoldr (Just . f) s)
