814387a5d5f28e3641d46d3c864d9876ced442e8
Koji Miyazato <viercc@gmail.com>
2021-04-26 22:10:49 +0900

    refactor and comment

--- unzip-leak/src/unzip-test.hs
+++ unzip-leak/src/unzip-test.hs
@@ -1,21 +1,40 @@
 {-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE CPP #-}
 module Main(main) where
 
-import Common
-import Data.List(unzip)
+import Data.List (unzip, foldl')
+import qualified Data.Vector.Unboxed as V
 
-f :: ([Vec], [Int]) -> (Int, Int)
-f (as, bs) = (vecMax $ sumVec as, bs !! 10)
+type Vec = V.Vector Int
 
-unzipFunctor :: Functor f => f (a,b) -> (f a, f b)
-unzipFunctor fab = (fst <$> fab, snd <$> fab)
+-- Set the CPP parameter UNZIP_FUNCTION to either
+-- 'unzipList' or 'unzipFunctor'
+main :: IO ()
+main = do
+  let (x,y) = summarize $ UNZIP_FUNCTION $ take 10_000 $ setup 10_000
+  print x
+  print y
 
 unzipList :: [(a,b)] -> ([a], [b])
 unzipList = unzip
 
-main :: IO ()
-main = do
-  let (x,y) = f $ UNZIP_FUNCTION $ take 10_000 $ setup 10_000
-  print x
-  print y
+unzipFunctor :: Functor f => f (a,b) -> (f a, f b)
+unzipFunctor fab = (fst <$> fab, snd <$> fab)
+
+-------------------------------------------------
+
+setup :: Int -> [(Vec, Int)]
+setup n = loop v0
+  where
+    v0 = V.replicate n 0
+    loop !v = i `seq` (v, i) : loop (V.imap (+) v)
+      where
+        i = V.maxIndex v
+{-# NOINLINE setup #-}
+
+summarize :: ([Vec], [Int]) -> (Int, Int)
+summarize ([],_) = error "Must be nonempty list!"
+summarize (v:vs, maxIndices) = (sumOfMax, maxIndices !! 10)
+  where
+    sumOfMax = V.sum $ foldl' (V.zipWith max) v vs
