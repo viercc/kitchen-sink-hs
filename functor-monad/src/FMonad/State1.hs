109cad1fe0d336a578d0d8f09da65c1c856bc13f
Koji Miyazato <viercc@gmail.com>
2022-04-27 15:18:40 +0900

    Update to State1

--- functor-monad/src/FMonad/State1.hs
+++ functor-monad/src/FMonad/State1.hs
@@ -16,7 +16,7 @@
 module FMonad.State1 where
 
 import Data.Functor.Day
-import Data.Functor.Day.Curried
+import Functor.Day.Curried
 import FMonad
 
 newtype State1 s x a = State1 {
@@ -24,8 +24,21 @@ newtype State1 s x a = State1 {
     }
     deriving Functor
 
-instance Functor s => FFunctor (State1 s) where
+pureState :: x a -> State1 s x a
+pureState x = State1 $ unapplied x
+
+joinState :: State1 s (State1 s x) a -> State1 s x a
+joinState = State1 . ffmap applied . runState1 . ffmap runState1
+
+cisState :: (forall r. s r -> (s r, x a)) -> State1 s x a
+cisState f = State1 $ Curried $ \s op -> let (s', x) = f s in Day s' x op
+
+transState :: (forall r. s r -> (s a, x r)) -> State1 s x a
+transState f = State1 $ Curried $ \s op -> let (s', x) = f s in Day s' x (flip op)
+
+instance FFunctor (State1 s) where
     ffmap fg (State1 st) = State1 $ ffmap (ffmap fg) st
-instance Functor s => FMonad (State1 s) where
-    fpure fx = State1 $ Curried $ \s -> Day s fx id
+
+instance FMonad (State1 s) where
+    fpure = pureState
     fjoin = State1 . ffmap applied . runState1 . ffmap runState1
